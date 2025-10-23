77f95842ee441de6dfa94e2490e6bd0a7ae3e1a3
regielaroscain <143473592+regielaroscain@users.noreply.github.com>
2025-10-23 16:39:11 +0800

    Backup

---
+++ Laroscain.sln/Laroscain/EmployeesDB/Stored Procedures/CreateEmployee.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[CreateEmployee]
+    @FullName NVARCHAR(100),
+    @Position NVARCHAR(50),
+    @Salary DECIMAL(10,2),
+    @Department NVARCHAR(50)
+AS
+BEGIN
+    INSERT INTO Employees (FullName, Position, Salary, Department)
+    VALUES (@FullName, @Position, @Salary, @Department)
+END
+
